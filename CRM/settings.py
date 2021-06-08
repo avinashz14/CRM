20f7d4e2f6adb18042923ead25ca87bf78502d19
avinashz14 <prajapati143a@gmail.com>
2021-06-08 19:14:57 +0530

    modified main urls

--- CRM/settings.py
+++ CRM/settings.py
@@ -123,9 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'accounts/static')
 ]
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
